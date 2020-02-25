8053712a47803f3ce04a98ea479d9a14ddffa8ab
AdamPhilipSmith <adamphilipsmith@hotmail.co.uk>
2020-02-25 11:49:11 +0000

    All CRUD methods tested and working

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,4 +1,5 @@
 package controller;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.google.gson.Gson;
@@ -14,17 +15,63 @@ public class MainController {
 		
 		ArrayList<Film> films = fdao.getAllFilms();
 		
+		
+		
+		//Prints out all films to String
 		for(int i=0; i<films.size(); i++) {
 			Film oneFilm = films.get(i);
-			//System.out.println(oneFilm.toString());
+			System.out.println(oneFilm.toString());
 		}
+		
+		
+		//Prints out all films to JSON
 		Gson gson = new Gson();
 		String allFilmsJson = gson.toJson(films);
-		//System.out.println(allFilmsJson);
+		System.out.println(allFilmsJson);
 		
-		Film f = fdao.getFilmByID(10013);
-		System.out.println(f.toString());
-		System.out.println(gson.toJson(f));
+		
+		//Prints out a film searched by ID
+		Film byID = fdao.getFilmByID(10013);
+		System.out.println(byID.toString());
+		
+		
+		//Prints out all films containing the chosen title
+		ArrayList<Film> searchedFilms = fdao.getFilmByTitle("wars");
+		
+		for(int i=0; i<searchedFilms.size(); i++) {
+		Film searchedFilm = searchedFilms.get(i);
+				System.out.println(searchedFilm.toString());
+		}
+		
+		
+		
+		
+		//Inserts a film to the database and prints out an int to say if it was successful or not.
+		Film insert = new Film (99, "test", 1984, "test", "test",
+			"test");
+	
+		System.out.println(fdao.insertFilm(insert));
+		
+		
+		
+		
+		//Updates a films ID with a new ID. Printing out an int to say if this was successful.
+		System.out.println(fdao.updateID(88, 99));
+		
+		
+		
+			
+		
+		//Deletes the film with the given ID. Printing out an int to say if this was successful.
+		try {
+			System.out.println(fdao.deleteFilm(88));
+		} catch (SQLException e) {
+			 
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 }
--- src/model/FilmDAO.java
+++ src/model/FilmDAO.java
@@ -1,37 +1,42 @@
 package model;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.sql.*;
 
-
 public class FilmDAO {
-	
+
 	Film oneFilm = null;
 	Connection conn = null;
-    Statement stmt = null;
+	Statement stmt = null;
 	String user = "smitha";
-    String password = "Moudpoil5";
-    // Note none default port used, 6306 not 3306
-    String url = "jdbc:mysql://mudfoot.doc.stu.mmu.ac.uk:6306/"+user;
+	String password = "Moudpoil5";
+	// Note none default port used, 6306 not 3306
+	String url = "jdbc:mysql://mudfoot.doc.stu.mmu.ac.uk:6306/" + user;
 
-	public FilmDAO() {}
+	public FilmDAO() {
+	}
 
-	
-	private void openConnection(){
+	private void openConnection() {
 		// loading jdbc driver for mysql
-		try{
-		    Class.forName("com.mysql.jdbc.Driver").newInstance();
-		} catch(Exception e) { System.out.println(e); }
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
 		// connecting to database
-		try{
+		try {
 			// connection string for demos database, username demos, password demos
- 			conn = DriverManager.getConnection(url, user, password);
-		    stmt = conn.createStatement();
-		} catch(SQLException se) { System.out.println(se); }	   
-    }
-	private void closeConnection(){
+			conn = DriverManager.getConnection(url, user, password);
+			stmt = conn.createStatement();
+		} catch (SQLException se) {
+			System.out.println(se);
+		}
+	}
+
+	private void closeConnection() {
 		try {
 			conn.close();
 		} catch (SQLException e) {
@@ -40,68 +45,147 @@ public class FilmDAO {
 		}
 	}
 
-	private Film getNextFilm(ResultSet rs){
-    	Film thisFilm=null;
+	private Film getNextFilm(ResultSet rs) {
+		Film thisFilm = null;
 		try {
-			thisFilm = new Film(
-					rs.getInt("id"),
-					rs.getString("title"),
-					rs.getInt("year"),
-					rs.getString("director"),
-					rs.getString("stars"),
-					rs.getString("review"));
+			thisFilm = new Film(rs.getInt("id"), rs.getString("title"), rs.getInt("year"), rs.getString("director"),
+					rs.getString("stars"), rs.getString("review"));
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	return thisFilm;		
+		return thisFilm;
 	}
-	
-	
-	
-   public ArrayList<Film> getAllFilms(){
-	   
+
+	public ArrayList<Film> getAllFilms() {
+
 		ArrayList<Film> allFilms = new ArrayList<Film>();
 		openConnection();
-		
-	    // Create select statement and execute it
-		try{
-		    String selectSQL = "select * from films";
-		    ResultSet rs1 = stmt.executeQuery(selectSQL);
-	    // Retrieve the results
-		    while(rs1.next()){
-		    	oneFilm = getNextFilm(rs1);
-		    	allFilms.add(oneFilm);
-		   }
 
-		    stmt.close();
-		    closeConnection();
-		} catch(SQLException se) { System.out.println(se); }
+		// Create select statement and execute it
+		try {
+			String query = "select * from films";
+			ResultSet rs1 = stmt.executeQuery(query);
+			// Retrieve the results
+			while (rs1.next()) {
+				oneFilm = getNextFilm(rs1);
+				allFilms.add(oneFilm);
+			}
 
-	   return allFilms;
-   }
+			stmt.close();
+			closeConnection();
+		} catch (SQLException se) {
+			System.out.println(se);
+		}
 
-   public Film getFilmByID(int id){
-	   
+		return allFilms;
+	}
+
+	public Film getFilmByID(int id) {
+
 		openConnection();
-		oneFilm=null;
-	    // Create select statement and execute it
-		try{
-		    String selectSQL = "select * from films where id="+id;
-		    ResultSet rs1 = stmt.executeQuery(selectSQL);
-	    // Retrieve the results
-		    while(rs1.next()){
-		    	oneFilm = getNextFilm(rs1);
-		    }
+		oneFilm = null;
+		// Create select statement and execute it
+		try {
+			String query = "select * from films where id=" + id;
+			ResultSet rs1 = stmt.executeQuery(query);
+			// Retrieve the results
+			while (rs1.next()) {
+				oneFilm = getNextFilm(rs1);
+			}
 
-		    stmt.close();
-		    closeConnection();
-		} catch(SQLException se) { System.out.println(se); }
+			stmt.close();
+			closeConnection();
+		} catch (SQLException se) {
+			System.out.println(se);
+		}
 
-	   return oneFilm;
-   }
-   
-   
-   
-   
+		return oneFilm;
+	}
+	
+	public ArrayList<Film> getFilmByTitle(String title) {
+		
+		ArrayList<Film> searchedFilms = new ArrayList<Film>();
+		openConnection();
+		oneFilm = null;
+		// Create select statement and execute it
+		try {
+			String query = "select * from films where title like '%" + title + "%';";
+			ResultSet rs1 = stmt.executeQuery(query);
+			// Retrieve the results
+			while (rs1.next()) {
+				oneFilm = getNextFilm(rs1);
+				searchedFilms.add(oneFilm);
+			}
+
+			stmt.close();
+			closeConnection();
+		} catch (SQLException se) {
+			System.out.println(se);
+		}
+
+		return searchedFilms;
+	}
+
+	public int insertFilm(Film film) {
+		int i = 0;
+		openConnection();
+
+		try {
+			String query = "INSERT INTO films (id, title, year, director, stars, review) VALUES (" + film.getId() + ",'"
+					+ film.getTitle() + "'," + film.getYear() + ",'" + film.getDirector() + "','" + film.getStars()
+					+ "','" + film.getReview() + "');";
+			// Inserts the films and returns integer. 1 being success and 0 being error.
+			i = stmt.executeUpdate(query);
+
+			stmt.close();
+			closeConnection();
+		} catch (SQLException se) {
+			System.out.println(se);
+		}
+
+		return i;
+
+	}
+
+	public int updateID(int newID, int ID) {
+
+		int i = 0;
+		openConnection();
+
+		try {
+
+			String query = "UPDATE films " + "SET id = " + newID + " WHERE id = " + ID + ";";
+
+			i = stmt.executeUpdate(query);
+
+			stmt.close();
+			closeConnection();
+		} catch (SQLException se) {
+			System.out.println(se);
+		}
+
+		return i;
+	}
+
+	public int deleteFilm(int film_id) throws SQLException {
+
+		openConnection();
+		int i = 0;
+
+		String query = "DELETE FROM films WHERE id = " + film_id + ";";
+
+		try {
+
+			i = stmt.executeUpdate(query);
+			stmt.close();
+			closeConnection();
+
+		} catch (SQLException se) {
+			System.out.println(se);
+		}
+
+		return i;
+	}
+
 }
